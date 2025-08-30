0cbefa8b7efd221521a88e21af7cb1ef3531505a
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-08-30 08:19:40 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Asp.Versioning;
+using Sufra.Infrastructure.ExceptionHandling;
+
+namespace SlyceAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+            builder.Services.AddProblemDetails();
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1.0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new UrlSegmentApiVersionReader(),
+                    new HeaderApiVersionReader("X-Version")
+                );
+                options.ReportApiVersions = true;
+            }
+            ).AddMvc();
+
+            var app = builder.Build();
+
+
+            app.UseExceptionHandler();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
