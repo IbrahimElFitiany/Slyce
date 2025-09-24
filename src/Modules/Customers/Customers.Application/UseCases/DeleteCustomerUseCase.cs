142020c7044adfb033b109116172812402a159ab
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-09-24 04:27:08 +0300

    refactor(customers): replace service with use-case classes
    - Removed ICustomerService and CustomerService
    - Added DeleteCustomerUseCase and RegisterCustomerUseCase
    - Moves towards a use-case–driven architecture

---
+++ src/Modules/Customers/Customers.Application/UseCases/DeleteCustomerUseCase.cs
@@ -0,0 +1,24 @@
+﻿using Customers.Domain.Entites;
+using Customers.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers.Application.UseCases
+{
+    public class DeleteCustomerUseCase
+    {
+        private readonly ICustomerRepository _repository;
+        public DeleteCustomerUseCase(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Execute(string id)
+        {
+            await _repository.DeleteAsync(Guid.Parse(id));
+        }
+    }
+}
