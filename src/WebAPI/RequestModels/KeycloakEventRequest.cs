02ab68d038fa0cdb522ce961b24610696d80f99f
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-10-01 16:42:28 +0300

    feat(webhook): add Keycloak webhook controller and request model
    - Added KeycloakWebhookController to handle incoming Keycloak events
    - Added KeycloakEventRequest as the request model for model binding
    - Removed obsolete CustomerController

---
+++ src/WebAPI/RequestModels/KeycloakEventRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAPI.RequestModels
+{
+    public class KeycloakEventRequest
+    {
+        public required string EventId { get; set; }
+        public long Time { get; set; }
+        public required string Type { get; set; }
+        public required string ClientId { get; set; }
+        public required string UserId { get; set; }
+        public string? Fname  { get; set; }
+        public string? Lname { get; set; }
+        public string? Email { get; set; }
+        public required string Role { get; set; }
+    }
+}
