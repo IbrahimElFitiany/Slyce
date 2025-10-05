95d853c5205085f04ef3dd418cda4f6675767815
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-10-05 20:36:00 +0300

    feat(restaurants-infra): add DbContext, entity configurations, and initial migration
    - Added RestaurantDbContext for module database context
    - Added RestaurantConfiguration for entity setup
    - Created initial EF Core migration (InitRestaurantTable)
    - Updated project file and added DI registration via ServiceCollectionExtensions

---
+++ src/Modules/Restaurants/Restaurants.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.Interfaces;
+using Restaurants.Application.UseCases;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Repositories;
+
+namespace Restaurants.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRestaurantModule(this IServiceCollection services ,IConfiguration configuration)
+        {
+            services.AddDbContext<RestaurantDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IRestaurantRepository, EFRestaurantRepository>();
+
+            return services;
+        }
+    }
+}
