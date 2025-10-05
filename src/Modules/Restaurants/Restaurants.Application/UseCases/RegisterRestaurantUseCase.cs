d59332024f8393e14123aa3ea89bead9a1dcc0c0
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-10-05 23:48:06 +0300

    feat(restaurants-application-layer): add restaurant registration use case and related DTOs
    - Added RegisterRestaurantRequest and RegisterRestaurantResponse DTOs
    - Added IRestaurantRepository interface for data access abstraction
    - Implemented RegisterRestaurantUseCase to handle registration logic
    - Updated Restaurants.Application.csproj to include new files

---
+++ src/Modules/Restaurants/Restaurants.Application/UseCases/RegisterRestaurantUseCase.cs
@@ -0,0 +1,22 @@
+﻿using Restaurants.Application.Interfaces;
+using Restaurants.Application.DTOs;
+using Restaurants.Domain.Entities;
+namespace Restaurants.Application.UseCases
+{
+    public class RegisterRestaurantUseCase
+    {
+        private readonly IRestaurantRepository _repository;
+        public RegisterRestaurantUseCase(IRestaurantRepository restaurantRepository)
+        {
+            _repository = restaurantRepository;
+        }
+
+        public async Task<RegisterRestaurantResponse> ExecuteAsync(RegisterRestaurantRequest restaurantRequest,CancellationToken cancellationToken)
+        {
+            var restaurant = new Restaurant(restaurantRequest.Name, restaurantRequest.Description, restaurantRequest.Type);
+            await _repository.AddAsync(restaurant, cancellationToken);
+            return new RegisterRestaurantResponse(restaurant.Id , restaurant.Name, restaurant.Description, restaurant.Type.ToString(),restaurant.Status.ToString());
+        }
+
+    }
+}
