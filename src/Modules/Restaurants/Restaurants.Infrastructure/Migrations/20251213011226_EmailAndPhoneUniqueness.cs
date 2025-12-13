855a573c31ef695633c1772b599e7054a3c8826d
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-12-13 03:18:13 +0200

    feat(RestaurantApplication): add unique indexes for CompanyEmail and MobileNumber in EF entity configuration

---
+++ src/Modules/Restaurants/Restaurants.Infrastructure/Migrations/20251213011226_EmailAndPhoneUniqueness.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailAndPhoneUniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantApplications_CompanyEmail",
+                schema: "restaurants",
+                table: "RestaurantApplications",
+                column: "CompanyEmail",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantApplications_MobileNumber",
+                schema: "restaurants",
+                table: "RestaurantApplications",
+                column: "MobileNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RestaurantApplications_CompanyEmail",
+                schema: "restaurants",
+                table: "RestaurantApplications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RestaurantApplications_MobileNumber",
+                schema: "restaurants",
+                table: "RestaurantApplications");
+        }
+    }
+}
