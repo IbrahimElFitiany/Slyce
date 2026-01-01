e35be881d5be1d490dd9a6d5f66515740f1f8947
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-01-01 04:32:22 +0200

    feat(Nutrition): add macro validation and calorie check
    - Prevent negative calories and macros
    - Ensure macro calories do not exceed total calories

--- src/Modules/Menus/Menus.Domain/ValueObjects/Nutrition.cs
+++ src/Modules/Menus/Menus.Domain/ValueObjects/Nutrition.cs
@@ -9,6 +9,9 @@
         public int SodiumMg { get; }
         public int SugarGrams { get; }
 
+        private const int caloriesPerGramOfFat = 9;
+        private const int caloriesPerGramOfProtein = 4;
+        private const int caloriesPerGramOfCarb = 4;
         private Nutrition() { }
         public Nutrition(
             int calories,
@@ -18,6 +21,16 @@
             int sodiumMg = 0,
             int sugarGrams = 0)
         {
+            if (calories < 0)
+                throw new ArgumentException("Calories cannot be negative");
+
+            if (proteinGrams < 0 || carbGrams < 0 || fatGrams < 0)
+                throw new ArgumentException("Macros cannot be negative");
+
+            var totalMacroCalories = proteinGrams * caloriesPerGramOfProtein + carbGrams * caloriesPerGramOfCarb + fatGrams * caloriesPerGramOfFat;
+
+            if (totalMacroCalories > calories)
+                throw new ArgumentException("Macros exceed total calories");
 
             Calories = calories;
             ProteinGrams = proteinGrams;
