acc388c7836997881c49557dc753dd5cfd4a40dd
Ibrahim ElFitiany <ibrahimelfitiany@gmail.com>
2026-01-03 04:57:19 +0200

    feat(customers): add basic RegisterCustomer handler (draft) (#16)

---
+++ src/Modules/Customers/Customers.Application/DTOs/CreateCustomerReqDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Customers.Application.DTOs
+{
+    public record CreateCustomerReqDTO(
+        string fname,
+        string lname,
+        string email,
+        string phoneNumber,
+        string password,
+        DateOnly birthDay
+    );
+}
--- src/Modules/Customers/Customers.Application/Interfaces/ICustomerRepository.cs
+++ src/Modules/Customers/Customers.Application/Interfaces/ICustomerRepository.cs
@@ -4,10 +4,10 @@ namespace Customers.Application.Interfaces
 {
     public interface ICustomerRepository
     {
-        Task<Customer?> GetByIdAsync(Guid id);
-        Task AddAsync(Customer customer);
-        Task<IEnumerable<Customer>> ListAsync();
-        Task DeleteAsync(Guid id);
-        Task SaveChangesAsync();
+        Task<Customer?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task AddAsync(Customer customer, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Customer>> ListAsync(CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
