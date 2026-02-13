35e28e8fdbeea87da90438485ed596c88a236bbc
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-02-13 19:25:42 +0200

    feat(menus-presentation): add basic controller and presentation layer

---
+++ src/Modules/Menus/Menus.Presentation/Controllers/MealsController.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Menus.Application.UseCases.Commands.CreateMenuMeal;
+using Menus.Presentation.DTOs;
+
+namespace Menus.Presentation.Controllers
+{
+    [Route("api/v{version:apiVersion}/meals")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class MealsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public MealsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateMenuMealReqDTO dto, CancellationToken ct)
+        {
+            Guid dummyUserId = Guid.NewGuid();
+            Guid mockUpRestaurant = Guid.Parse("ee7a56b2-eded-4745-9f7f-b13a02dce23f");
+
+            var command = new CreateMenuMealCommand(
+                dummyUserId,
+                mockUpRestaurant,
+                dto.CategoryId,
+                dto.Name,
+                dto.Description,
+                dto.ImgUrl,
+                dto.Ingredients,
+                dto.Sizes.Select(size =>
+                    new MealSizeInput(
+                        size.Name,
+                        size.Price,
+                        size.SortOrder,
+                        size.IngredientQuantity.Select(iq =>
+                            new IngredientQuantityInput(
+                                iq.IngredientId,
+                                iq.Quantity
+                            )
+                        ).ToList()
+                    )
+                ).ToList()
+            );
+            await _mediator.Send(command, ct);
+            return Ok();
+        }
+    }
+}
---
+++ src/Modules/Menus/Menus.Presentation/DTOs/CreateMenuMealReq.cs
@@ -0,0 +1,21 @@
+﻿namespace Menus.Presentation.DTOs
+{
+    public record CreateMenuMealReqDTO(
+        Guid CategoryId,
+        string Name,
+        string Description,
+        string ImgUrl,
+        List<Guid> Ingredients,
+        List<MealSizeDto> Sizes
+        );
+    public record MealSizeDto(
+        string Name,
+        decimal Price,
+        int SortOrder,
+        List<IngredientQuantityDto> IngredientQuantity
+        );
+    public record IngredientQuantityDto(
+        Guid IngredientId,
+        decimal Quantity
+        );
+}
