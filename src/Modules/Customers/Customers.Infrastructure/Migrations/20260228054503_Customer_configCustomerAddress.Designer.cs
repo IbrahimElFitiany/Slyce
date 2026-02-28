99735939f1d416a0f82ecf4a2a8764208976bb2b
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-03-01 00:56:49 +0200

    feat(customers-infra): add CustomerAddress EF config and migrations

--- src/Modules/Customers/Customers.Infrastructure/Migrations/20260228054503_Customer_configCustomerAddress.Designer.cs
+++ src/Modules/Customers/Customers.Infrastructure/Migrations/20260228054503_Customer_configCustomerAddress.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Customers.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Customers.Infrastructure.Migrations
 {
     [DbContext(typeof(CustomersDbContext))]
-    partial class CustomersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260228054503_Customer_configCustomerAddress")]
+    partial class Customer_configCustomerAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -88,9 +91,6 @@ namespace Customers.Infrastructure.Migrations
 
                             b1.Property<Guid>("CustomerId")
                                 .HasColumnType("uuid");
-
-                            b1.Property<bool>("IsPrimary")
-                                .HasColumnType("boolean");
 
                             b1.Property<string>("Label")
                                 .IsRequired()
