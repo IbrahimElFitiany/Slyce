daa0de61f4a591178930331b9baa257f6a258f84
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-04-10 08:26:17 +0200

    feat(menus): add AddMealSizeCommand & its validator

---
+++ src/Modules/Menus/Menus.Application/UseCases/Commands/AddMealSize/AddMealSizeCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Menus.Application.UseCases.Commands.AddMealSize
+{
+    public sealed record AddMealSizeCommand(
+        Guid MealId,
+        string Name,
+        decimal Price,
+        int SortOrder,
+        IEnumerable<IngredientQuantityInput> IngredientQuantities) : IRequest<Guid>;
+
+    public sealed record IngredientQuantityInput(Guid IngredientId, decimal Quantity);
+}
