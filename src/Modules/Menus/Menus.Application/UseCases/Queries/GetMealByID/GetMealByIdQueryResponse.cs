2ee81041e08c827b2a01f997e6d3a0f0da0b35c2
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-04-13 13:58:57 +0200

    feat(menus): add GetMealById query and controller endpoint

---
+++ src/Modules/Menus/Menus.Application/UseCases/Queries/GetMealByID/GetMealByIdQueryResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace Menus.Application.UseCases.Queries.GetMealByID
+{
+    public sealed record GetMealByIdQueryResponse(
+        Guid MealId,
+        string Name,
+        string Description,
+        string ImageUri,
+        bool Avalible,
+        IReadOnlyList<MealSizeResponse> Sizes);
+
+    public sealed record MealSizeResponse(
+        Guid SizeId,
+        string SizeName,
+        decimal Price,
+        string Currency,
+        int SortOrder,
+        IReadOnlyList<MealIngredientResponse> Ingredients,
+        NutritionResponse Nutrition);
+    public sealed record MealIngredientResponse(string IngredientName,decimal Qty);
+    public sealed record NutritionResponse(
+        decimal Calories,
+        decimal Protein,
+        decimal TotalFat,
+        decimal SaturatedFat,
+        decimal TransFat,
+        decimal Cholesterol,
+        decimal SodiumMg,
+        decimal TotalCarbohydrate,
+        decimal DietaryFiber,
+        decimal SugarGrams,
+        decimal VitaminD,
+        decimal CalciumMg,
+        decimal IronMg,
+        decimal PotassiumMg,
+        decimal VitaminAMcg,
+        decimal VitaminCMg);
+}
