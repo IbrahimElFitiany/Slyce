9f76c84494e69ba40e03a2d749e33bbf67b6f99a
Ibrahim ElFitiany <ibrahimelfitiany@gmail.com>
2026-04-15 18:33:50 +0200

    feat(menus): implement removeMealSize usecase (#44)

---
+++ src/Modules/Menus/Menus.Application/UseCases/Commands/RemoveMealSize/RemoveMealSizeCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Menus.Application.UseCases.Commands.RemoveMealSize
+{
+    public sealed record RemoveMealSizeCommand (Guid MealId, Guid SizeId) : IRequest;
+}
---
+++ src/Modules/Menus/Menus.Domain/Exceptions/MealSizeNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Domain.Exceptions;
+
+namespace Menus.Domain.Exceptions
+{
+    public sealed class MealSizeNotFoundException : DomainException
+    {
+        public MealSizeNotFoundException(Guid sizeId) : base($"Meal Size with Id '{sizeId}' was not found.") { }
+    }
+}
