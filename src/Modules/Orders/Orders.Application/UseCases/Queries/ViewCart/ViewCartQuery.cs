5a1c306049cfefabf86e2161ff4c2f0e40e85a88
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-04-24 16:25:46 +0300

    feat(orders): add viewCart endpoint, and query class

---
+++ src/Modules/Orders/Orders.Application/UseCases/Queries/ViewCart/ViewCartQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Orders.Application.UseCases.Queries.ViewCart
+{
+    public sealed record ViewCartQuery(Guid CustomerId) : IRequest<ViewCartQueryResponse>;
+
+    public sealed record ViewCartQueryResponse(IReadOnlyCollection<CartItemResponse> CartItems);
+    public sealed record CartItemResponse(
+        string Id,
+        string ImgUrl,
+        string Name,
+        string Description,
+        int Quantity,
+        decimal OriginalPrice,
+        decimal? DiscountedPrice);
+}
