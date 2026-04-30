d3295eef28ed78d135a3eccddfb4f88814dcb35e
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-04-30 07:07:37 +0300

    feat(food): implement SearchFoodSummary query with validation
    - Add SearchFoodSummaryQueryHandler in Infrastructure using ILike + pagination
    - Add SearchFoodSummaryValidator with FluentValidation
    - Register handler and validator pipeline behavior in DI

---
+++ src/Modules/Food/Food.Application/UseCases/Queries/SearchFoodSummaryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Food.Application.UseCases.Queries
+{
+    public sealed class SearchFoodSummaryValidator : AbstractValidator<SearchFoodSummaryQuery>
+    {
+        public SearchFoodSummaryValidator()
+        {
+            RuleFor(x => x.Term)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.PageNumber)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PageSize)
+                .GreaterThan(0)
+                .LessThanOrEqualTo(50);
+        }
+    }
+}
