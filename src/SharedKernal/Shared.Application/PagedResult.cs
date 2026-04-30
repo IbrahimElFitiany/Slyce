1b46a1c71eacf20acfd37a23e9352e72a83b750b
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-04-30 06:58:32 +0300

    feat(food): add search food summary endpoint
    - add SearchFoodSummaryQuery and result
    - add endpoint in FoodController
    - refactor FoodController to primary constructor
    - introduce shared PagedResult

---
+++ src/SharedKernal/Shared.Application/PagedResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Application
+{
+    public sealed record PagedResult<T>
+    (
+        int Total,
+        int PerPage,
+        int CurrentPage,
+        int TotalPages,
+        bool HasNext,
+        bool HasPrevious,
+        IReadOnlyList<T> Items);
+}
