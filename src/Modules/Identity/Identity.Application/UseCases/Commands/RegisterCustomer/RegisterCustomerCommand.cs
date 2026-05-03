844c5c0ad204c2fd642c0059bec325d21dbf074a
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-05-03 13:37:53 +0300

    feat(identity): add customer registration command and endpoint

---
+++ src/Modules/Identity/Identity.Application/UseCases/Commands/RegisterCustomer/RegisterCustomerCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Identity.Application.UseCases.Commands.RegisterCustomer
+{
+    public sealed record RegisterCustomerCommand(
+        string FirstName,
+        string LastName,
+        string Email,
+        string Password,
+        string PhoneNumber,
+        DateOnly BirthDay) : IRequest<Guid>;
+}
---
+++ src/Modules/Identity/Identity.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning;
+using Identity.Application.UseCases.Commands.RegisterCustomer;
+using Identity.Presentation.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Presentation.Controllers
+{
+    [Route("api/v{version:apiVersion}")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public sealed class UsersController (IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost("customers")]
+        public async Task<IActionResult> RegisterCustomer([FromBody] RegisterCustomerRequest request, CancellationToken ct)
+        {
+            var command = new RegisterCustomerCommand(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.Password,
+                request.PhoneNumber,
+                request.BirthDay
+            );
+
+            var result = await _mediator.Send(command, ct);
+
+            return Created(string.Empty, new { CustomerId = result} );
+        }
+    }
+}
