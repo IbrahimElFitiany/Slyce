5d47dbc3c55c07106bbc333adbe954b34bc596fb
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-05-09 20:48:02 +0300

    feat(customers): add onboarding gender update flow

---
+++ src/Modules/Customers/Customers.Application/UseCases/Commands/UpdateGender/UpdateGenderCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Customers.Application.UseCases.Commands.UpdateGender
+{
+    public sealed record UpdateGenderCommand(Guid CustomerId, string Gender) : IRequest;
+}
---
+++ src/Modules/Customers/Customers.Application/UseCases/Commands/UpdateGender/UpdateGenderCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Customers.Application.Interfaces;
+using Customers.Domain.Enums;
+using MediatR;
+using Shared.Application.Exceptions;
+
+namespace Customers.Application.UseCases.Commands.UpdateGender
+{
+    internal sealed class UpdateGenderCommandHandler(
+        IUnitOfWork unitOfWork,
+        ICustomerRepository customerRepository) : IRequestHandler<UpdateGenderCommand>
+    {
+        public async Task Handle(UpdateGenderCommand command, CancellationToken ct)
+        {
+            var customer = await customerRepository.GetByIdAsync(command.CustomerId, ct)
+                ?? throw new NotFoundException("Customer", command.CustomerId);
+
+            Enum.TryParse<Gender>(command.Gender, out var gender);
+
+            customer.UpdateGender(gender);
+
+            await unitOfWork.SaveChangesAsync(ct);
+        }
+    }
+}
