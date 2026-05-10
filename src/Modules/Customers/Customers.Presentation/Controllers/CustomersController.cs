f5e8750120bc6889b1489f9eea1285a41a104196
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-05-10 20:12:17 +0300

    chore(customers): remove delete customer use case

--- src/Modules/Customers/Customers.Presentation/Controllers/CustomersController.cs
+++ src/Modules/Customers/Customers.Presentation/Controllers/CustomersController.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using Asp.Versioning;
-using Customers.Application.UseCases.Commands.DeleteCustomer;
 using Customers.Application.UseCases.Commands.UpdateGender;
 using Customers.Presentation.DTOs;
 using Microsoft.AspNetCore.Authorization;
@@ -19,13 +18,6 @@ namespace Customers.Presentation.Controllers
         public Task<IActionResult> GetCustomer([FromRoute] Guid id, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCustomer([FromRoute] Guid id, CancellationToken cancellationToken)
-        {
-            await mediator.Send(new DeleteCustomerCommand(id), cancellationToken);
-            return Ok();
         }
 
         [HttpPatch("me/profile/gender")]
