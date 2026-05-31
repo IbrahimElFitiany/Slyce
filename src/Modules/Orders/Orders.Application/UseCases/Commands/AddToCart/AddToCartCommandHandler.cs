5845ac8afe51970389172999dd3c112e80de0431
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-05-31 06:13:52 +0300

    feat(orders): update application handlers to use branch-based business logic

--- src/Modules/Orders/Orders.Application/UseCases/Commands/AddToCart/AddToCartCommandHandler.cs
+++ src/Modules/Orders/Orders.Application/UseCases/Commands/AddToCart/AddToCartCommandHandler.cs
@@ -11,29 +11,26 @@ namespace Orders.Application.UseCases.Commands.AddToCart
         ICartRepository cartRepository,
         IUnitOfWork unitOfWork) : IRequestHandler<AddToCartCommand>
     {
-        private readonly IMenuQueryServices _menuQueryServices = menuQueryServices;
-        private readonly ICartRepository _cartRepository = cartRepository;
-        private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
         public async Task Handle(AddToCartCommand command, CancellationToken ct)
         {
-            var meal = await _menuQueryServices.GetMealSummaryAsync(command.MealId, ct) ??
-                throw new NotFoundException("MenuMeal", command.MealId);
+            var meal = await menuQueryServices.GetMealSummaryAsync(command.MealId, ct) 
+                ?? throw new NotFoundException("MenuMeal", command.MealId);
 
             if (!meal.MealSizeIds.Contains(command.SizeId))
                 throw new NotFoundException("MealSize", command.SizeId);
 
-            var cart = await _cartRepository.GetCartByCustomerIdAsync(command.CustomerId, ct);
+            var cart = await cartRepository.GetCartByCustomerIdAsync(command.CustomerId, ct);
 
             if (cart is null)
             {
                 cart = new Cart(command.CustomerId, meal.RestaurantId);
-                _cartRepository.Add(cart);
+                cartRepository.Add(cart);
             }
 
             cart.AddCartItem(command.MealId, command.SizeId, command.Quantity, meal.RestaurantId);
 
-            await _unitOfWork.SaveChangesAsync(ct);
+            await unitOfWork.SaveChangesAsync(ct);
         }
     }
 }
