795839e28a4cf72b0f52a6a02133aa8879fbf9fd
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-06-04 16:22:22 +0300

    feat(orders): add contract layer

---
+++ src/Modules/Orders/Orders.Contract/DTOs/OrderDTO.cs
@@ -0,0 +1,11 @@
+﻿using Orders.Contract.Interfaces;
+
+namespace Orders.Contract.DTOs
+{
+    public sealed record OrderDTO(
+        Guid CustomerId,
+        Guid BranchId,
+        IEnumerable<OrderItemDTO> OrderItemDTOs);
+
+    public sealed record OrderItemDTO(Guid MealId, Guid SizeId, int Qty);
+}
---
+++ src/Modules/Orders/Orders.Contract/Interfaces/IOrderServices.cs
@@ -0,0 +1,9 @@
+﻿using Orders.Contract.DTOs;
+
+namespace Orders.Contract.Interfaces
+{
+    public interface IOrderServices
+    {
+        Task CreateOrdersAsync(IEnumerable<OrderDTO> orderDTOs, CancellationToken ct);
+    }
+}
