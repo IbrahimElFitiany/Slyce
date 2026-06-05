67ff529f04ba11e0e14cc73340662e145abeecdf
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-06-05 23:29:56 +0300

    feat(subscriptions): add and implement GetActiveSubscriptions repository method

--- src/Modules/Subscriptions/Subscriptions.Infrastructure/Repositories/EFSubscriptionRepository.cs
+++ src/Modules/Subscriptions/Subscriptions.Infrastructure/Repositories/EFSubscriptionRepository.cs
@@ -1,30 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Subscriptions.Application.Interfaces;
 using Subscriptions.Domain.Entities;
+using Subscriptions.Domain.Enums;
 using Subscriptions.Infrastructure.Persistence;
 
 namespace Subscriptions.Infrastructure.Repositories
 {
-    public class EFSubscriptionRepository : ISubscriptionRepository
+    internal sealed class EFSubscriptionRepository(SubscriptionsDbContext dbContext) : ISubscriptionRepository
     {
-        private readonly SubscriptionsDbContext _dbContext;
 
-        public EFSubscriptionRepository(SubscriptionsDbContext db)
+        public void Add(Subscription subscription) => dbContext.Add(subscription);
+
+        public void Delete(Subscription subscription) => dbContext.Remove(subscription);
+        public async Task<Subscription?> GetByIdAsync(Guid id, CancellationToken ct = default) => await dbContext.Subscriptions.FindAsync(id, ct);
+
+        public Task<List<Subscription>> GetAllActiveAsync(CancellationToken ct = default)
         {
-            _dbContext = db;
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            return dbContext.Subscriptions
+                .Where(
+                s => s.Status == SubscriptionStatus.Active &&
+                s.StartDate <= today &&
+                s.EndDate >= today &&
+                s.DeliveryDays.Any(d => d.Day == today.DayOfWeek)).ToListAsync(ct);
         }
 
-        public void Add(Subscription subscription)
-        {
-            _dbContext.Add(subscription);
-        }
-        public void Delete(Subscription subscription)
-        {
-            _dbContext.Remove(subscription);
-        }
-        public async Task<Subscription?> GetByIdAsync(Guid id, CancellationToken ct = default)
-        {
-            return await _dbContext.Subscriptions.FindAsync(id, ct);
-        }
+
     }
 }
