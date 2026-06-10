f5484053459b8d08ba5e9cec22501d5277045b25
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-06-11 00:54:22 +0300

    merge dev into staging

---
+++ src/Modules/Menus/Menus.Infrastructure/Queries/GetRestaurantCategoriesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Menus.Application.UseCases.Queries.GetRestaurantCategories;
+using Menus.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menus.Infrastructure.Queries
+{
+    internal sealed class GetRestaurantCategoriesQueryHandler(
+        MenusDbContext dbContext) : IRequestHandler<GetRestaurantCategoriesQuery, GetRestaurantCategoriesQueryResponse>
+    {
+        public async Task<GetRestaurantCategoriesQueryResponse> Handle(GetRestaurantCategoriesQuery query, CancellationToken cancellationToken)
+        {
+            var result = await dbContext.MenuCategories
+                .Where(c => c.RestaurantId == query.RestaurantId)
+                .Select(c => new CategoryItem(c.Id, c.Name))
+                .ToListAsync(cancellationToken);
+
+            return new GetRestaurantCategoriesQueryResponse(result);
+        }
+    }
+}
--- src/Modules/Menus/Menus.Presentation/Controllers/CategoriesController.cs
+++ src/Modules/Menus/Menus.Presentation/Controllers/CategoriesController.cs
@@ -3,6 +3,9 @@ using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using Menus.Application.UseCases.Commands.CreateMenuCategory;
 using Menus.Presentation.DTOs;
+using Shared.Presentation;
+using Menus.Application.UseCases.Queries.GetRestaurantCategories;
+using Microsoft.AspNetCore.Authorization;
 
 
 namespace Menus.Presentation.Controllers
@@ -10,7 +13,7 @@ namespace Menus.Presentation.Controllers
     [Route("api/v{version:apiVersion}/restaurants/{restaurantId}/categories")]
     [ApiVersion("1.0")]
     [ApiController]
-    public sealed class CategoriesController(IMediator mediator) : ControllerBase
+    public sealed class CategoriesController(IMediator mediator) : BaseController
     {
 
         [HttpPost]
@@ -21,6 +24,17 @@ namespace Menus.Presentation.Controllers
         {
             var categoryId = await mediator.Send(new CreateMenuCategoryCommand(createCategoryReq.Name,restaurantId), ct);
             return Created(string.Empty, new { categoryId });
+        }
+
+        [HttpGet]
+        [Authorize (Roles = "RestaurantOwner")]
+        public async Task<IActionResult> GetCategories([FromRoute] Guid restaurantId, CancellationToken ct)
+        {
+            if (restaurantId != RestaurantId)
+                return Forbid();
+
+            var result = await mediator.Send(new GetRestaurantCategoriesQuery(restaurantId), ct);
+            return Ok(result);
         }
     }
 }
