4324626e385fd0366125b0a25d6b9aeb08f17ecd
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-06-11 00:49:39 +0300

    feat(menus): add get restaurant categories query

---
+++ src/Modules/Menus/Menus.Application/UseCases/Queries/GetRestaurantCategories/GetRestaurantCategoriesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Menus.Application.UseCases.Queries.GetRestaurantCategories
+{
+    public sealed record GetRestaurantCategoriesQuery(Guid RestaurantId) : IRequest<GetRestaurantCategoriesQueryResponse>;
+
+    public sealed record GetRestaurantCategoriesQueryResponse(IReadOnlyList<CategoryItem> Categories);
+    public sealed record CategoryItem(Guid CategoryId, string Name);
+}
