9c6cd7370c93785ce763ce578c28ed1bd98ad8d8
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-06-11 21:12:03 +0300

    merge dev into staging

--- src/Modules/Restaurants/Restaurants.Presentation/DTOs/AddBranchRequest.cs
+++ src/Modules/Restaurants/Restaurants.Presentation/DTOs/AddBranchRequest.cs
@@ -1,7 +1,6 @@
 ﻿namespace Restaurants.Presentation.DTOs
 {
     public sealed record AddBranchRequest(
-        Guid RestaurantId,
         string BranchName,
         string BranchContactNumber,
         string City,
