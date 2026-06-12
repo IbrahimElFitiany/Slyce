fd45e16caa693a2199ea31895933662c8a1c7e29
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-06-13 01:32:37 +0300

    merge dev into staging

---
+++ src/Modules/Menus/Menus.Application/UseCases/Queries/SearchMealsNearBy/SearchNearbyMealsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Menus.Application.UseCases.Queries.SearchMealsNearBy
+{
+    public sealed record SearchNearbyMealsQuery(string SearchTerm, double Lng , double Lat) : IRequest<SearchMealsNearByResponse>;
+    public sealed record SearchMealsNearByResponse(IReadOnlyList<MealSummary> Meals);
+    public sealed record MealSummary(
+        Guid MealId,
+        string MealImageUrl,
+        string MealName,
+        double LowestCalorieOption,
+        string Currency,
+        decimal Price);
+
+}
