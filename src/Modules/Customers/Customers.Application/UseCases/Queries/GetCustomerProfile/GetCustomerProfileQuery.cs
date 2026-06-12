e55e579bc49202920a571a3448c9b6998c6153fa
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2026-06-13 02:02:28 +0300

    feat(customers): implement get customer profile query
    This feature is implemented for demo purposes only.

---
+++ src/Modules/Customers/Customers.Application/UseCases/Queries/GetCustomerProfile/GetCustomerProfileQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Customers.Application.UseCases.Queries.GetCustomerProfile
+{
+    public sealed record GetCustomerProfileQuery(Guid CustomerId) : IRequest<CustomerProfileResponse>;
+
+    public sealed record CustomerProfileResponse(
+        string? Gender,
+        decimal? WeightKg,
+        int? HeightCm,
+        string ActivityRate,
+        IReadOnlyCollection<Guid> Allergens,
+        IReadOnlyCollection<Guid> DietPreferences);
+}
---
+++ src/Modules/Customers/Customers.Infrastructure/Queries/GetCustomerProfileQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Customers.Application.UseCases.Queries.GetCustomerProfile;
+using Customers.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared.Application.Exceptions;
+using System.Linq;
+
+namespace Customers.Infrastructure.Queries
+{
+    internal sealed class GetCustomerProfileQueryHandler(
+        CustomersDbContext customersDbContext) : IRequestHandler<GetCustomerProfileQuery, CustomerProfileResponse>
+    {
+        public async Task<CustomerProfileResponse> Handle(GetCustomerProfileQuery query, CancellationToken cancellationToken)
+        {
+            var profile = await customersDbContext.Customers
+                .AsNoTracking()
+                .Where(c => c.Id == query.CustomerId)
+                .Select(c => new CustomerProfileResponse(
+                    c.Gender.ToString(),
+                    c.Weight.Kilograms,
+                    c.Height.Centimeters,
+                    c.ActivityRate.ToString(),
+                    c.Allergens,
+                    c.DietPreferences))
+                .FirstOrDefaultAsync(cancellationToken)
+                ?? throw new NotFoundException("Customer", query.CustomerId);
+
+            return profile;
+        }
+    }
+}
